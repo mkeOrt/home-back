b890336abc6edd4f8f2d68cec19f6a0603eab171
Mike <mke.ort@gmail.com>
2022-07-17 17:08:16 -0500

    fixing lint

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 
